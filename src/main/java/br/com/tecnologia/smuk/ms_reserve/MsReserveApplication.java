2527baaef0e2baaf4d3504adac78c3919913d569
Alexandre Ferreira Costa <alexandre.costa@sankhya.com.br>
2025-05-27 14:58:07 -0300

    chore: Configurando projeto

---
+++ src/main/java/br/com/tecnologia/smuk/ms_reserve/MsReserveApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnologia.smuk.ms_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecnologia/smuk/ms_reserve/MsReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnologia.smuk.ms_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
